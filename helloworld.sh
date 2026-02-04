0cd03a41265dd10b3a5aa4aba42a838cfb73e942
Charan Kotineni <charan.kotineni@peopletech.com>
2026-02-04 12:51:40 +0530

    userinputs

--- helloworld.sh
+++ helloworld.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-echo "enter ur name"
-read USER_NAME
-echo "user name is $USER_NAME"
+time = $(date)
+echo "$time at this we played"
+time1= $(date +%s)
+sleep 10
+time2= $(date +%s)
+total= $(($time2-$time1))
+echo "the diff is $total"
