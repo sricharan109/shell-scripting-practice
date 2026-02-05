a981979d7cf14bf81ddb74352f6509269378c22b
Charan Kotineni <charan.kotineni@peopletech.com>
2026-02-05 10:55:36 +0530

    user

--- helloworld.sh
+++ helloworld.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 id=$(id -u)
-if [ id -ne 0]; then
-            echo "try with root user"
-            exit 1
-fi
-ANSER(){
-      if [ $1 -ne 0 ]; then
-             echo "$2 Installing..........FAILED"
-             exit 1
-      else
-             echo "Installing ...Success"
-      fi
 
-      }
-dhf install nginx
-ANSER $? "Installing nginx"
+if [ $id -ne 0 ]; then
+    echo "Try with root user"
+    exit 1
+fi
+
+ANSWER() {
+    if [ $1 -ne 0 ]; then
+        echo "$2 .... FAILED"
+        exit 1
+    else
+        echo "$2 .... SUCCESS"
+    fi
+}
+
+dnf install nginx -y
+ANSWER $? "Installing nginx"
